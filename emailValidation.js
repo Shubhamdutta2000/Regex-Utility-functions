1dcd1503a8d60f3f7bd1287a72f65394c057d427
Lana Fidelis <lanamenezesfidelis@gmail.com>
2021-10-10 21:26:51 -0300

    Couple improvements on email validation

--- emailValidation.js
+++ emailValidation.js
@@ -1,5 +1,16 @@
 // Email Validation with regex
-const reg2 = /^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/;
-const input = "shubham@gmail.com"
 
-const check3 = reg2.test(input) // returns true
+validateEmail("testing@test.com"); // Must be a string
+
+function validateEmail(email) {
+  //make sure its a string and guarantee it's lowercase
+  var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+    String(email).toLowerCase()
+  );
+  if (email.match(emailFormat)) {
+    alert("Cool email!");
+    return true;
+  }
+  alert("Is this an email account?");
+  return false;
+}
